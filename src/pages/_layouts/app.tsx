cbc0140d4b8feaae8f206d12ffa67d0884ce5e30
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-01-03 18:20:28 -0300

    feat: create components layouts and configure in router

---
+++ src/pages/_layouts/app.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom'
+
+function AppLayout() {
+  return (
+    <div>
+      <h1>Cabeçalho</h1>
+
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default AppLayout
